e65b691a8300526c5839e67f177dbc6fa333392e
Gabriel B. Santos <>
2020-12-07 10:31:00 -0300

    Restructured the code tree
    Put the code in `src/tetris` and add references for other assets:
    examples, documentation, and tests.
    
    Also, the commit improves the module usability.

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-"""Python module to create structured meshes with OpenFOAM's blockMesh."""
-
-from .mesh import Block, Edge, Mesh, Patch, Vertex
-
-__all__ = [Vertex, Edge, Patch, Block, Mesh]
